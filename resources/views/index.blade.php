776bda61eccca73f5b2e81273eb93e35c4779848
nathanvercaemert <nathanv@xternsoftware.com>
2019-10-18 11:45:15 -0400

    saved reformatted table and reintroduced previous projects list

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -23,28 +23,7 @@
         </div>
         <div class="row">
             <div class="col overflow-auto mb-3 " style="height:500;">
-                <div class="row">
-                    <div class="col">Customer</div>
-                    <div class="col">cid</div>
-                    <div class="col">Project</div>
-                </div>
-
-                @foreach ( $data[ 'results' ][ 'projects' ] as $project )
-                    <div class="row">
-                        <div class="col">{{ $project[ 'customer' ][ 'name' ] }}</div>
-                        <div class="col">{{ $project[ 'customer' ][ 'custrowid' ] }}</div>
-                        <div class="col">
-                            <button type="button" class="btn btn-link font-weight-bold" @click="gettasks( '{{ $project[ 'projrowid' ] }}' );
-                                                                                                getprojectcustomer( '{{ $project[ 'projrowid' ] }}' );
-                                                                                                getprojectstatus( '{{ $project[ 'projrowid' ] }}' );
-                                                                                                setprojrowid( '{{ $project[ 'projrowid' ] }}' );
-                                                                                                populatetaskmodal( '{{ $project[ 'projrowid' ] }}' );">
-                                {{ $project[ 'title' ] }}
-                            </button>
-                        </div>
-                    </div>
-                @endforeach
-
+                @include( 'tables.projects' )
             </div>
             <div class="col overflow-auto mb-3" style="height:500;">
                 <tasks-component v-bind:tasks="tasks" :taskrowidadd="taskrowidadd" :projstatusrowid="projstatusrowid" :projtyperowid="projtyperowid"></tasks-component>

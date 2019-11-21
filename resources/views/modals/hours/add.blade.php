6a2e44823fc24c1ff7e24768db390f4410bb410a
nathanvercaemert <nathanv@xternsoftware.com>
2019-11-21 10:25:56 -0500

    date picker added to add hour modal

--- resources/views/modals/hours/add.blade.php
+++ resources/views/modals/hours/add.blade.php
@@ -30,7 +30,7 @@
                   </div>
                   <div class="col-md-4 col-sm-6 col-xs-12 input-padding">
                       <label for="dateentered">Date Entered</label>
-                      <input type="text" class="form-control" id="dateentered" name="dateentered" placeholder="yyyy-mm-dd">
+                      <input class="form-control" type="date" value="{{date('Y-m-d')}}" id="dateentered" name="dateentered">
                   </div>
                   <div class="col-md-4 col-sm-6 col-xs-12 input-padding">
                       <label for="notes">Notes</label>

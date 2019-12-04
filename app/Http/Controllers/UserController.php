38e5e4a9652234a4a4fb98b0367a601e4c610f30
nathanvercaemert <nathanv@xternsoftware.com>
2019-12-04 10:29:38 -0500

    user controller class updated to reflect task controller class

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -56,6 +56,43 @@ class UserController extends APIController
         return $this->return_success( $request, $pagevars );
     }
 
+    public function update( Request $request, $user_id )
+    {
+        $user_core = new TaskCore();
+
+        $param_list = $request->all();
+        $master_update_list = $user_core->fields_update_list();
+        $update_list = array();
+        $rec = false;
+        foreach ( $master_update_list as $value ) {
+            if( $request->input( $value ) == NULL_VALUE ) {
+                $update_list[ $value ] = null;
+            } else if ( isset( $param_list[ $value ] ) == true ) {
+                $param_value = $request->input( $value );
+                $update_list[ $value ] = $param_value;
+            }
+        }
+
+        if( !empty( $update_list ) ) {
+            $rec = $user_core->update( $user_id, $update_list );
+        }
+
+        if ( $rec === -1 || $rec === false || $rec === null ) {
+            return $this->return_error( $request );
+        }
+
+        return $this->return_success( $request );
+    }
+
+    public function delete( Request $request, $user_id )
+    {
+        $user_core = new UserCore();
+
+        $user_core->delete( $user_id );
+
+        return $this->return_success( $request );
+    }
+
     public function createnew( Request $request )
     {
         $user_core = new UserCore();
